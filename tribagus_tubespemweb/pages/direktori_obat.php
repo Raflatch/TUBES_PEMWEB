8579a23a5a137c9a40b69ce5bcef5cee47f082d8
Stellar Mohammad Tri Bagus <115438548+MohammadTriBagus@users.noreply.github.com>
2024-06-13 12:45:29 +0700

    Add files via upload
    Tambahan Direktori Obat dan Penyakit

---
+++ tribagus_tubespemweb/pages/direktori_obat.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Direktori Obat</title>
+    <link rel="stylesheet" href="../styles/style_obat.css">
+</head>
+<body>
+    <header>
+        <h1>Direktori Obat</h1>
+    </header>
+    <main>
+        <div class="card">
+            <img src="bipolar.png" alt="Bipolar Image">
+            <h2>Bipolar</h2>
+            <p>Gangguan bipolar adalah gangguan mental yang ditandai dengan perubahan emosi yang drastis. Seseorang yang menderita bipolar dapat merasakan gejala mania (sangat senang) dan depresi (sangat terpuruk).</p>
+            <footer>Gangguan Bipolar</footer>
+        </div>
+        <div class="card">
+            <img src="kecemasan_umum.png" alt="Kecemasan Umum Image">
+            <h2>Kecemasan Umum</h2>
+            <p>Gangguan kecemasan umum adalah kondisi saat seseorang mengalami kecemasan yang berlebihan dan berlangsung terus-menerus terhadap banyak hal.</p>
+            <footer>Gangguan Kecemasan Umum</footer>
+        </div>
+        <div class="card">
+            <img src="aboulia.png" alt="Aboulia Image">
+            <h2>Aboulia</h2>
+            <p>Merujuk pada kurangnya kemauan atau inisiatif dan dapat dilihat sebagai gangguan motivasi yang menurun (DDM).</p>
+            <footer>Gangguan Aboulia</footer>
+        </div>
+        <div class="card">
+            <img src="aboulia.png" alt="Aboulia Image">
+            <h2>Aboulia</h2>
+            <p>Merujuk pada kurangnya kemauan atau inisiatif dan dapat dilihat sebagai gangguan motivasi yang menurun (DDM).</p>
+            <footer>Gangguan Aboulia</footer>
+        </div>
+        <div class="card">
+            <img src="aboulia.png" alt="Aboulia Image">
+            <h2>Aboulia</h2>
+            <p>Merujuk pada kurangnya kemauan atau inisiatif dan dapat dilihat sebagai gangguan motivasi yang menurun (DDM).</p>
+            <footer>Gangguan Aboulia</footer>
+        </div>
+    </main>
+    <script src="scripts.js"></script>
+</body>
+</html>
